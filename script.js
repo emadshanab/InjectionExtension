6e89e9430e784a295818f7a87fa4eef06ac44057
Tomer Elmalem <telmalem@gmail.com>
2011-11-25 16:59:16 -0500

    Test the onclick functions for each context menu item.

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-var sql = chrome.contextMenus.create({"title": "SQL Injection"});
-var xss = chrome.contextMenus.create({"title": "XSS Injection"});
+$(document).ready(function() {
+  var sql = chrome.contextMenus.create({"title": "SQL Injection", "onclick": sql_injection});
+  var xss = chrome.contextMenus.create({"title": "XSS Injection", "onclick": xss_injection}); 
+
+  function xss_injection() {
+    alert('xss hello');
+  }
+
+  function sql_injection() {
+    alert('sql hello');
+  }
+});
+
+
