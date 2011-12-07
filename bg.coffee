a3756a41382ba5010148224d525108b84c490a45
Tomer Elmalem <telmalem@gmail.com>
2011-12-06 21:04:40 -0500

    Create a submenu for the SQL and XSS attacks.

--- bg.coffee
+++ bg.coffee
@@ -1,24 +1,30 @@
 jQuery ->
-  attacks = ["", "sql attack", "xss attack"]
-
-  chrome.contextMenus.create
+  sql_menu = chrome.contextMenus.create
     "title": "SQL Injection"
-    "onclick": (data) ->
-      console.log data.menuItemId
-      send_attack data.menuItemId
+
+  xss_menu = chrome.contextMenus.create
+    "title": "XSS Injection"
 
   chrome.contextMenus.create
-    "title": "XSS Injection"
+    "title": "Basic SQL Injection"
+    "parentId": sql_menu 
     "onclick": (data) ->
       console.log data.menuItemId
-      send_attack data.menuItemId
+      send_attack data.parentMenuItemId, data.menuItemId
 
-  send_attack = (type) ->
-    console.log "Sending attack: " + attacks[type]
+  chrome.contextMenus.create
+    "title": "Basic XSS Injection"
+    "parentId": xss_menu
+    "onclick": (data) ->
+      console.log data.menuItemId
+      send_attack data.parentMenuItemId, data.menuItemId
+
+  send_attack = (type, attack) ->
+    console.log "Sending attack. "
     chrome.tabs.getSelected null, (tab) ->
       chrome.tabs.sendRequest tab.id, 
         type: type
-        attack: attacks[type],
+        attack: attack
         (response) ->
           console.log response.acknowledged
 
--- bg.js
+++ bg.js
@@ -1,27 +1,34 @@
 
   jQuery(function() {
-    var attacks, send_attack;
-    attacks = ["", "sql attack", "xss attack"];
+    var send_attack, sql_menu, xss_menu;
+    sql_menu = chrome.contextMenus.create({
+      "title": "SQL Injection"
+    });
+    xss_menu = chrome.contextMenus.create({
+      "title": "XSS Injection"
+    });
     chrome.contextMenus.create({
-      "title": "SQL Injection",
+      "title": "Basic SQL Injection",
+      "parentId": sql_menu,
       "onclick": function(data) {
         console.log(data.menuItemId);
-        return send_attack(data.menuItemId);
+        return send_attack(data.parentMenuItemId, data.menuItemId);
       }
     });
     chrome.contextMenus.create({
-      "title": "XSS Injection",
+      "title": "Basic XSS Injection",
+      "parentId": xss_menu,
       "onclick": function(data) {
         console.log(data.menuItemId);
-        return send_attack(data.menuItemId);
+        return send_attack(data.parentMenuItemId, data.menuItemId);
       }
     });
-    return send_attack = function(type) {
-      console.log("Sending attack: " + attacks[type]);
+    return send_attack = function(type, attack) {
+      console.log("Sending attack. ");
       return chrome.tabs.getSelected(null, function(tab) {
         return chrome.tabs.sendRequest(tab.id, {
           type: type,
-          attack: attacks[type]
+          attack: attack
         }, function(response) {
           return console.log(response.acknowledged);
         });
