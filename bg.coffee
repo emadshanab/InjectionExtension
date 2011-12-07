4b31101483f799afec42ea79d60c5435d736c91f
Tomer Elmalem <telmalem@gmail.com>
2011-12-06 21:52:16 -0500

    Dynamically create the submenus.

--- bg.coffee
+++ bg.coffee
@@ -1,30 +1,36 @@
 jQuery ->
+  create_sub_menu = (parent, id, name) ->
+    chrome.contextMenus.create
+      "title": name
+      "parentId": parent 
+      "onclick": (data) ->
+        send_attack parent, id 
+
   sql_menu = chrome.contextMenus.create
     "title": "SQL Injection"
 
   xss_menu = chrome.contextMenus.create
     "title": "XSS Injection"
 
-  chrome.contextMenus.create
-    "title": "Basic SQL Injection"
-    "parentId": sql_menu 
-    "onclick": (data) ->
-      console.log data.menuItemId
-      send_attack data.parentMenuItemId, data.menuItemId
+  attacks = []
+  attacks['sql'] = [
+    {id: 0, name: "Basic SQL", parent: sql_menu},
+    {id: 1, name: "Intermediate SQL", parent: sql_menu}
+  ]
+  attacks['xss'] = [
+    {id: 0, name: "Basic XSS", parent: xss_menu},
+    {id: 1, name: "Intermediate XSS", parent: xss_menu}
+  ]
 
-  chrome.contextMenus.create
-    "title": "Basic XSS Injection"
-    "parentId": xss_menu
-    "onclick": (data) ->
-      console.log data.menuItemId
-      send_attack data.parentMenuItemId, data.menuItemId
+  for type in [attacks.sql, attacks.xss]
+    for attack in type
+      create_sub_menu(attack.parent, attack.id, attack.name)
 
   send_attack = (type, attack) ->
-    console.log "Sending attack. "
+    console.log "Sending attack." + type + ", " + attack
     chrome.tabs.getSelected null, (tab) ->
       chrome.tabs.sendRequest tab.id, 
         type: type
         attack: attack
         (response) ->
           console.log response.acknowledged
-
--- bg.js
+++ bg.js
@@ -1,30 +1,54 @@
 
   jQuery(function() {
-    var send_attack, sql_menu, xss_menu;
+    var attack, attacks, create_sub_menu, send_attack, sql_menu, type, xss_menu, _i, _j, _len, _len2, _ref;
+    create_sub_menu = function(parent, id, name) {
+      return chrome.contextMenus.create({
+        "title": name,
+        "parentId": parent,
+        "onclick": function(data) {
+          return send_attack(parent, id);
+        }
+      });
+    };
     sql_menu = chrome.contextMenus.create({
       "title": "SQL Injection"
     });
     xss_menu = chrome.contextMenus.create({
       "title": "XSS Injection"
     });
-    chrome.contextMenus.create({
-      "title": "Basic SQL Injection",
-      "parentId": sql_menu,
-      "onclick": function(data) {
-        console.log(data.menuItemId);
-        return send_attack(data.parentMenuItemId, data.menuItemId);
+    attacks = [];
+    attacks['sql'] = [
+      {
+        id: 0,
+        name: "Basic SQL",
+        parent: sql_menu
+      }, {
+        id: 1,
+        name: "Intermediate SQL",
+        parent: sql_menu
       }
-    });
-    chrome.contextMenus.create({
-      "title": "Basic XSS Injection",
-      "parentId": xss_menu,
-      "onclick": function(data) {
-        console.log(data.menuItemId);
-        return send_attack(data.parentMenuItemId, data.menuItemId);
+    ];
+    attacks['xss'] = [
+      {
+        id: 0,
+        name: "Basic XSS",
+        parent: xss_menu
+      }, {
+        id: 1,
+        name: "Intermediate XSS",
+        parent: xss_menu
       }
-    });
+    ];
+    _ref = [attacks.sql, attacks.xss];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      type = _ref[_i];
+      for (_j = 0, _len2 = type.length; _j < _len2; _j++) {
+        attack = type[_j];
+        create_sub_menu(attack.parent, attack.id, attack.name);
+      }
+    }
     return send_attack = function(type, attack) {
-      console.log("Sending attack. ");
+      console.log("Sending attack." + type + ", " + attack);
       return chrome.tabs.getSelected(null, function(tab) {
         return chrome.tabs.sendRequest(tab.id, {
           type: type,
